c83c46fcb2ae0b0521781c09fc694dbe161931cd
Bruno Drugowick <bruno.drugowick@gmail.com>
2020-04-15 22:52:11 -0300

    Fix permissions.
    (API is not accessible give csrf configuration)

--- src/main/java/dev/drugowick/securingapiandpages/config/WebSecurityConfig.java
+++ src/main/java/dev/drugowick/securingapiandpages/config/WebSecurityConfig.java
@@ -15,19 +15,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .inMemoryAuthentication()
-                .withUser("user").password("{noop}password").roles("USER").and()
-                .withUser("admin").password("{noop}password").roles("ADMIN").and()
-                .withUser("api").password("{noop}password").roles("API");
+                .withUser("user").password("{noop}password").roles("BASIC", "API_BASIC").and()
+                .withUser("admin").password("{noop}password").roles("ADMIN", "API_ADMIN");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                .antMatchers(("/api/**")).hasRole("API")
+                .antMatchers("/api/roles/**").hasRole("API_ADMIN")
+                .antMatchers("/api/users/**").hasAnyRole("API_BASIC", "API_ADMIN")
                 .antMatchers("/roles/**").hasRole("ADMIN")
-                .antMatchers("/users/**").hasAnyRole("ADMIN", "USER")
+                .antMatchers("/users/**").hasAnyRole("ADMIN", "BASIC")
                 .antMatchers("/").permitAll()
+                .and().httpBasic()
                 .and().formLogin();
     }
 }
